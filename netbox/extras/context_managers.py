d779de4dec707f89554c05e79cf525bf1cf34b12
Tyler Bigler <tyler_bigler@homedepot.com>
2021-11-01 12:04:37 -0400

    Remove thread lock and instead use request-unique dispatch UIDs

--- netbox/extras/context_managers.py
+++ netbox/extras/context_managers.py
@@ -1,6 +1,6 @@
+import threading
 from contextlib import contextmanager
 
-import threading
 from django.db.models.signals import m2m_changed, pre_delete, post_save
 
 from extras.signals import clear_webhooks, _clear_webhook_queue, _handle_changed_object, _handle_deleted_object
@@ -21,28 +21,25 @@ def change_logging(request):
 
     webhook_queue = []
 
-    with changelog_lock:
+    # Curry signals receivers to pass the current request
+    handle_changed_object = curry(_handle_changed_object, request, webhook_queue)
+    handle_deleted_object = curry(_handle_deleted_object, request, webhook_queue)
+    clear_webhook_queue = curry(_clear_webhook_queue, webhook_queue)
 
-        # Curry signals receivers to pass the current request
-        handle_changed_object = curry(_handle_changed_object, request, webhook_queue)
-        handle_deleted_object = curry(_handle_deleted_object, request, webhook_queue)
-        clear_webhook_queue = curry(_clear_webhook_queue, webhook_queue)
+    # Connect our receivers to the post_save and post_delete signals.
+    post_save.connect(handle_changed_object, dispatch_uid=f'handle_changed_object_{request.id}')
+    m2m_changed.connect(handle_changed_object, dispatch_uid=f'handle_changed_object_{request.id}')
+    pre_delete.connect(handle_deleted_object, dispatch_uid=f'handle_deleted_object_{request.id}')
+    clear_webhooks.connect(clear_webhook_queue, dispatch_uid=f'clear_webhook_queue_{request.id}')
 
-        # Connect our receivers to the post_save and post_delete signals.
-        post_save.connect(handle_changed_object, dispatch_uid='handle_changed_object')
-        m2m_changed.connect(handle_changed_object, dispatch_uid='handle_changed_object')
-        pre_delete.connect(handle_deleted_object, dispatch_uid='handle_deleted_object')
-        clear_webhooks.connect(clear_webhook_queue, dispatch_uid='clear_webhook_queue')
+    yield
 
-        yield
-
-        # Disconnect change logging signals. This is necessary to avoid recording any errant
-        # changes during test cleanup.
-        post_save.disconnect(handle_changed_object, dispatch_uid='handle_changed_object')
-        m2m_changed.disconnect(handle_changed_object, dispatch_uid='handle_changed_object')
-        pre_delete.disconnect(handle_deleted_object, dispatch_uid='handle_deleted_object')
-        clear_webhooks.disconnect(clear_webhook_queue, dispatch_uid='clear_webhook_queue')
-
+    # Disconnect change logging signals. This is necessary to avoid recording any errant
+    # changes during test cleanup.
+    post_save.disconnect(handle_changed_object, dispatch_uid=f'handle_changed_object_{request.id}')
+    m2m_changed.disconnect(handle_changed_object, dispatch_uid=f'handle_changed_object_{request.id}')
+    pre_delete.disconnect(handle_deleted_object, dispatch_uid=f'handle_deleted_object_{request.id}')
+    clear_webhooks.disconnect(clear_webhook_queue, dispatch_uid=f'clear_webhook_queue_{request.id}')
 
     # Flush queued webhooks to RQ
     flush_webhooks(webhook_queue)
